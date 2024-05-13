f05a30a02791a84786c61413ad3a2cd8f6f02edd
Joao Bezerra <jbezerra@integrationmatters.com>
2024-05-13 22:47:27 +0200

    make dynamically pinnable Toast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, inject, ViewChild} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Toast, ToastModule} from "primeng/toast";
+import {ButtonModule} from "primeng/button";
+import {MessageService} from "primeng/api";
+import {RippleModule} from "primeng/ripple";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToastModule, ButtonModule, RippleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-toast-project';
+  // @ts-ignore
+  @ViewChild(Toast) toast: Toast;
+
+  private readonly messageService: MessageService=  inject(MessageService);
+
+  show() {
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Message Content', key:'test-toggle-toast' });
+  }
+
+  onToggleSticky() {
+    this.toast.messages?.filter(m => m.key === "test-toggle-toast").forEach(m => m.sticky = !m.sticky)
+  }
 }
